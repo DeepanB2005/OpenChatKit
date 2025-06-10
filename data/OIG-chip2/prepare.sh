902b9b9796640917222c061f02f870971cf816ef
DEEPAN . B <deepanb2005@gmail.com>
2025-06-10 18:47:25 +0530

    Update prepare.sh

--- data/OIG-chip2/prepare.sh
+++ data/OIG-chip2/prepare.sh
@@ -1,2 +1,2 @@
 DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-wget https://huggingface.co/datasets/laion/OIG/resolve/main/unified_chip2.jsonl -O ${DIR}/unified_chip2.jsonl
+wget https://huggingface.co/datasets/laion/OIG/resolve/main/unified_chip2.jsonl -O ${DIR}/unifie
